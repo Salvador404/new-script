fbdebcc649339858bf89443fb66bff068138761d
Salvador404 <yasermhmddoost@gmail.com>
2026-08-12 23:00:19 +0330

    Correcting the loop error

--- script.sh
+++ script.sh
@@ -4,13 +4,17 @@ read -p "First enter Promodo time you think suitable for you(by the minute) : " 
 read -p "now enter the rest time(by the minute) : " rest
 read -p "Enter the number of Promodo you want : " count
 echo "Evrything is ready! press ENTER to start . . ."
-read 
+read
+echo "here we go . . ."
 for((i=0;i<${count};i++));do 
-    sleep ${promodo}m
+    
+    sleep ${promodo}s
     echo "Its time to rest . . . press ENTER to start rest timer"
     read 
-    sleep ${rest}m
+    sleep ${rest}s
+    tput clear
     echo "Get ready to start again! press ENTER to start next promodo"
+    read
 done
 echo "GLHF!!!bye bye . . ."
 sleep 3
